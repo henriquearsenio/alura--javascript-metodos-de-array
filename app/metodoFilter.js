ece794f612861e25a865cc9c593f0b5580ae5cad
henriquearsenio <henrique.severiano@live.com>
2023-08-14 21:39:08 -0300

    finalizado

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -4,6 +4,27 @@ botoes.forEach(btn => btn.addEventListener('click', filtrarLivros))
 function filtrarLivros() {
     const elementoBtn = document.getElementById(this.id)
     const categoria = elementoBtn.value
-    let livrosFiltrados = livros.filter(livro => livro.categoria == categoria)
+    let livrosFiltrados = categoria == 'disponivel' ? filtrarPorDisponibilidade() : filtrarPorCategoria(categoria)
     exibirOsLivrosNaTela(livrosFiltrados)
+    if (categoria == 'disponivel') {
+        const ValorTotal = calcularValorTotalDeLivrosDisponiveis(livrosFiltrados)
+
+        exibirValorTotalDosLivrosDisponiveisNaTela(ValorTotal)
+    }
+}
+
+function filtrarPorCategoria(categoria) {
+    return livros.filter(livro => livro.categoria == categoria)
+}
+
+function filtrarPorDisponibilidade() {
+    return livros.filter(livro => livro.quantidade > 0)
+}
+
+function exibirValorTotalDosLivrosDisponiveisNaTela(ValorTotal) {
+    elementoComValorTotalDeLivrosDisponiveis.innerHTML = `
+        <div class="livros__disponiveis">
+        <p>Todos os livros disponíveis por R$ <span id="valor">${ValorTotal}</span></p>
+        </div>
+    `
 }
